9b6566098eb57558c4fc188d3f92d9e9fe59214e
keweizhou <1514991931@qq.com>
2021-03-31 00:09:01 +0800

    并行流

---
+++ src/main/java/com/javatools/javatools/concurrency/ForkJoinDemo.java
@@ -0,0 +1,20 @@
+package com.javatools.javatools.concurrency;
+
+import java.util.stream.LongStream;
+
+/**
+ * Description
+ * ForkJoin 在1.7，并行执行任务！提高速率，大数据量
+ * 特点：工作窃取，原因是里面维护的是双端队列
+ * Date 2021/3/30 22:53
+ * Created by kwz
+ */
+public class ForkJoinDemo {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        long sum = LongStream.rangeClosed(0L, 10_000_000L).parallel().reduce(0, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println("sum=" + sum + "时间：" + (end - start));
+    }
+}
