ad7b02a727123dd127018f51c91794f39434352d
keweizhou <1514991931@qq.com>
2021-08-08 22:52:04 +0800

    Validation Failed: 1: type is missing;2: type is missing

--- springboot-es-shopping/src/main/java/com/kwz/springbootesshopping/controller/BookController.java
+++ springboot-es-shopping/src/main/java/com/kwz/springbootesshopping/controller/BookController.java
@@ -1,7 +1,15 @@
 package com.kwz.springbootesshopping.controller;
 
+import com.kwz.springbootesshopping.service.IBookService;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 /**
  * Description
@@ -10,6 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
+@RequestMapping("/books")
 public class BookController {
 
+    @Setter(onMethod_ = @Autowired)
+    private IBookService bookService;
+
+    @PostMapping("/status/{keyWords}")
+    public Boolean saveBooks(@PathVariable String keyWords) {
+        try {
+            return bookService.parseBooks(keyWords);
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("save books error", e);
+        }
+        return false;
+    }
 }
